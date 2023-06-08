b1aa7700b37796db44b83e3caa9791c3d5ee4a4b
breno possagnolo <110348128+breno369@users.noreply.github.com>
2023-06-07 22:18:26 -0300

    cabecalho.h
    definição das funções

---
+++ cabecalho.h
@@ -0,0 +1,20 @@
+#ifndef CABECALHO_H
+#define CABECALHO_H
+
+char ler_operacao ();
+
+double ler ();
+
+double resto (double, double);
+
+void imprimir(double);
+
+double soma (double, double);
+
+double subtracao (double, double);
+
+double multiplicacao (double, double);
+
+double divisao (double, double);
+
+#endif
